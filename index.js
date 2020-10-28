6a51a1da18d0741ab7df43b9bc2edebd8b262b77
Digitalworldlover <gm.gmbh.ch@gmail.com>
2020-10-28 12:46:59 +0300

    files

---
+++ index.js
@@ -0,0 +1,36 @@
+// let name = 'selo'; // sting
+// var last = 'hissam';
+// let interestRate = 1;// numberliteral
+// let isApproved = false; // boolean literal
+// let firsName = undefined;
+// let lastName = null;
+// let person = {
+//     name: 'selo',
+//     age: 28
+// };
+// person.name = 'mosh';
+// person['name'] = 'josh';
+// let selection = 'name'
+// person[selection] = 'Mary';
+
+// console.log(person.name);
+
+// alert('Hi,Salahuddin');
+//this is my javascript comment
+// console.log('Hi,darling');
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green';
+// selectedColors[3] = 3;
+// console.log(selectedColors.length);
+
+// function great(name, lastName) {
+//     console.log('Hello ' + name + ' ' + lastName);
+// }
+
+// function sqaure(number) {
+//     return number * number;
+// }
+
+// let number = square(2);
+// console.log(number);
